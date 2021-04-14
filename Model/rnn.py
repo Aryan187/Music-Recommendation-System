6b37cb4229a1dfe82ae8c2acd25d162f1bacb632
Rachit Mittal <49209329+rachitmittal24@users.noreply.github.com>
2021-04-15 02:50:48 +0530

    Update rnn.py

--- Model/rnn.py
+++ Model/rnn.py
@@ -2,15 +2,10 @@ from torch.nn import Module
 import torch.nn as nn
 class RNN(Module):
 
-    def __init__(self, n_in, num_layers, n_hidden, n_out):
+    def __init__(self, num_in, num_layers, num_hidden, num_out):
         super(RNN, self).__init__()
-        self.lstm = nn.LSTM(n_in, n_hidden, num_layers, dropout=0.3)
-        self.body = nn.Sequential(
-            nn.Linear(n_hidden, n_hidden),
-            nn.Dropout(0.3),
-            nn.Linear(n_hidden, n_out),
-            nn.ReLU()
-        )
+        self.lstm = nn.LSTM(num_in, num_hidden, num_layers, dropout=0.3)
+        self.body = nn.Sequential(nn.Linear(num_hidden, num_hidden),nn.Dropout(0.3),nn.Linear(num_hidden, n_out),nn.ReLU())
 
     def forward(self, X):
         X, _ = self.lstm(X)
