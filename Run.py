b2f7394dc423598bb81cfeab1339f36e80d12986
Aryan187 <mt1180744@iitd.ac.in>
2021-05-13 11:44:33 +0530

    working code, not correct

--- Run.py
+++ Run.py
@@ -2,26 +2,33 @@ import numpy as np
 import pandas as pd
 import torch
 import csv
-from Encoding.Encode import music_encode, user_encode, user_dynamic_encode
+from Encoding.Encode import music_encode, user_encode, user_dynamic_encode, lonehotenc
 from DataCleaning.Creating_unique_list import unique_artists, unique_composers, unique_genre, unique_language, unique_user_features
 from Model.dnn import DNN
+from torch.utils.data import DataLoader
 ###rac add
 from Model.rnn import RNN
+from train import DTNMR, DTNMRWrapper
+from dataset import MRSDataset
 #####
-def build_dict (train,songs):
+def build_dict (train,songs,users):
 	user_songs={}
+	train_points=[]
 	for index, row in train.iterrows():
+		if row['msno'] in users['msno'].tolist() and row['song_id'] in songs['song_id'].tolist():
+			train_points.append([row['msno'],row['song_id']])
 		if row['song_id'] in songs['song_id'].tolist():
 			if row['msno'] not in user_songs.keys():
 				user_songs[row['msno']]=[]
 			user_songs[row['msno']].append([row['song_id'],row['source_system_tab']])
-	return user_songs
+	return user_songs, train_points
 
 songs = pd.read_csv('DataCleaning/final_songs.csv')
 users = pd.read_csv('DataCleaning/final_users.csv')
 train = pd.read_csv('DataCleaning/final_train.csv')
 #dict_file = pd.read_csv('DataCleaning/user_song_behaviour_dict.csv')
-usb_dict = build_dict(train,songs)
+train['source_system_tab'].fillna('none', inplace=True)
+usb_dict, train_points = build_dict(train,songs,users)
 print("Data Loading Done")
 genre = unique_genre(songs)
 artist = unique_artists(songs)
@@ -30,22 +37,20 @@ language = unique_language(songs)
 encoded_music = music_encode(songs,genre,artist,composer,language)
 #print(encoded_music['NV9HhUzyK50tGvxb3w0PdZoaw3Ypp86XDmmMr0vgFdg='])
 print("Music Encoding Done")
-
-model = DNN(9939)
-music_char = {}
-for song in songs.song_id:
-	music_char[song] = model.forward(torch.FloatTensor(encoded_music[song]))
-print("Music passed through DNN")
+# model = DNN(9939)
+# music_char = {}
+# for song in songs.song_id:
+# 	music_char[song] = model.forward(torch.FloatTensor(encoded_music[song]))
+# print("Music passed through DNN")
 
 city, age, gender = unique_user_features(users)
 encoded_user_intrinsic = user_encode(users,gender,city,age,usb_dict,encoded_music)
 print("User Intrinsic Encoding Done")
-
-model2 = DNN(9972)
-user_int_char = {}
-for user in users.msno:
-	user_int_char[user] = model2.forward(torch.FloatTensor(encoded_user_intrinsic[user]))
-print("User Instrinsic passed through DNN")
+# model2 = DNN(9972)
+# user_int_char = {}
+# for user in users.msno:
+# 	user_int_char[user] = model2.forward(torch.FloatTensor(encoded_user_intrinsic[user]))
+# print("User Instrinsic passed through DNN")
 
 
 ###########rachit add##############
@@ -60,15 +65,29 @@ for i in train.index:
 behaviour=train['source_system_tab'].tolist()
 behaviour=np.unique(np.array(behaviour))
 behaviour=behaviour.tolist()
+benum = {k:i for i, k in enumerate(behaviour)}
+behaviour_enc = {}
+for b in behaviour:
+	behaviour_enc[b] = lonehotenc(benum,b)
 ###
-encoded_user_dynamic=user_dynamic_encode(users,data,music_char,behaviour)
-print("User dynamic Encoding Done")
-modelRNN=RNN(9+32,2,256,32)
-###RNN model -  num_in, num_layers, num_hidden, num_out
-modelRNN=modelRNN.double() ### takes input in double not float
-###
-user_dyn_char = {}
-for user in users.msno:
-	user_dyn_char[user] = modelRNN.forward((torch.tensor(encoded_user_dynamic[user][-10:]).unsqueeze(1)).double()) + modelRNN.forward((torch.tensor(encoded_user_dynamic[user][-20:]).unsqueeze(1)).double())
-print("User dynamic passed through RNN")
+# encoded_user_dynamic=user_dynamic_encode(users,data,music_char,behaviour)
+# print("User dynamic Encoding Done")
+# modelRNN=RNN(9+32,2,256,32)
+# ###RNN model -  num_in, num_layers, num_hidden, num_out
+# modelRNN=modelRNN.double() ### takes input in double not float
+# ###
+# user_dyn_char = {}
+# for user in users.msno:
+# 	print(user)
+# 	user_st = (torch.tensor(encoded_user_dynamic[user][-10:]).unsqueeze(1)).double()
+# 	user_lt = (torch.tensor(encoded_user_dynamic[user][-20:]).unsqueeze(1)).double()
+# 	user_dyn_char[user] = modelRNN.forward(user_st) + modelRNN.forward(user_lt)
+# print("User dynamic passed through RNN")
 ###########################################
+train_set = MRSDataset(train_points,data,users,songs,encoded_user_intrinsic,encoded_music,behaviour_enc,4,10)
+train_dl = DataLoader(train_set, batch_size=16, shuffle=True,collate_fn=lambda x: x)
+model = DTNMR(9972,9939,9,st_playlist_len=5,emb_size=32)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+system = DTNMRWrapper(model,train_dl,train_dl,optimizer)
+
+system.train(epochs=1)
---
+++ train.py
@@ -0,0 +1,104 @@
+from statistics import mean
+
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.nn import Module, Linear
+from tqdm import tqdm
+
+from Model.dnn import DNN
+from Model.rnn import RNN
+
+class DTNMRWrapper:
+
+    def __init__(self, model, train_dl, valid_dl, optimizer):
+        self.model = model
+        self.train_dl = train_dl
+        self.valid_dl = valid_dl
+        self.optimizer = optimizer
+        self.criterion = nn.CrossEntropyLoss()
+
+    def train(self, epochs):
+        for epoch in range(epochs):
+            print('Epoch %i/%i' % (epoch+1, epochs))
+            losses, accuracies = self.train_epoch()
+            print('summary of epoch: average loss={:.2f}, average accuracy={:.2f}' \
+                .format(mean(losses), mean(accuracies)))
+
+    def train_epoch(self):
+        losses, accuracies = [], []
+        for i, x in (t := tqdm(enumerate(self.train_dl), total=len(self.train_dl))):
+            #y = torch.zeros((np.array(x[0]).shape[0],1), dtype=torch.long) #? add , 1) if necessary
+            y = torch.zeros((1,1), dtype = torch.long)
+            y_hat = self.model(*x[0])
+
+            self.optimizer.zero_grad()
+            print(y)
+            loss = self.criterion(y_hat, y)
+            loss.backward()
+            self.optimizer.step()
+
+            loss = loss.item()
+            accuracy = (torch.argmax(y_hat, dim=1) == 0).float().mean().item()
+            losses.append(loss)
+            accuracies.append(accuracy)
+            t.set_description("loss %.2f - accuracy %.2f%%" % (loss, accuracy*100))
+        return losses, accuracies
+
+
+class DTNMR(Module):
+    """
+    The recommendation neural network is comprised of 4 components.
+        [USFC] User Static Feature Component
+            USFC(user_encoding) = user_static_embedding
+        [MFC] Music Feature Component:
+            song_embedding = MFC(song_encoding)
+        [UDFC] User Dynamic Feature Component: long-term & short-term
+            song_playlist_embedding = UDFC(song_embedding[])
+        [RC] Rating Component
+            rating = RC(user_embedding, song_embedding)
+    """
+
+    def __init__(self, user_mhe_size, song_mhe_size, behavior_mhe_size, st_playlist_len, emb_size):
+        super(DTNMR, self).__init__()
+        self.user_mhe_size = user_mhe_size
+        self.song_mhe_size = song_mhe_size
+        self.emb_size = emb_size
+        self.song_emb_size = emb_size + behavior_mhe_size
+        self.st_playlist_len = st_playlist_len
+
+        self.USFC = DNN(self.user_mhe_size)
+        self.MFC = DNN(self.song_mhe_size)
+        self.UDFC = RNN(self.song_emb_size, num_layers=2, num_hidden=256, num_out=emb_size)
+        self.RC = Linear(2*emb_size, 1)
+
+    def forward(self, user, playlist, behaviors, subset):
+        """
+        The forward pass of the model is the calculation of the rating between the user and
+        each song in the subset. The result is the list [Score(u, s) for s in subset].
+        """
+        # User static feature embedding.
+        user_static = self.USFC((torch.tensor(user)).float())
+        # User dynamic feature embedding.
+        if (len(playlist) == 0):
+            latent_playlist = torch.zeros((1,32), dtype = torch.float)
+        else:
+            latent_playlist = torch.stack([self.MFC((torch.tensor(s)).float()) for s in playlist])
+        if (len(behaviors) == 0):
+            behaviors = torch.zeros((1,9), dtype = torch.float)
+        else:
+            behaviors = torch.stack([(torch.tensor(b)).float() for b in behaviors])
+        Lt_playlist = torch.cat([latent_playlist, behaviors], dim=1)
+        st_playlist = Lt_playlist[-self.st_playlist_len:]
+        user_dynamic = self.UDFC((Lt_playlist.unsqueeze(1)).float()) + self.UDFC((st_playlist.unsqueeze(1)).float())
+
+        # User feature combined embedding.
+        user_feature = user_static + user_dynamic
+        # Rating evaluation on song subset.
+        ratings = []
+        for song in subset:
+            embedding = self.MFC(torch.tensor(song).float())
+            combined = torch.cat([user_feature, embedding.unsqueeze(0)], dim=1)
+            ratings.append(self.RC(combined))
+
+        return torch.stack(ratings, dim=1)
