39321b3469f3c583f24335bc56f49f45ba981b26
Rachit Mittal <49209329+rachitmittal24@users.noreply.github.com>
2021-05-14 16:46:29 +0530

    Add files via upload

--- dataset.py
+++ dataset.py
@@ -2,20 +2,21 @@ import random
 from torch.utils.data import Dataset
 
 class MRSDataset(Dataset):
-    """
-    User u has a playlist [s_1, s_2, s_3, ..., s_n] ordered in time. When picking an entry in
-    the dataset, we randomly select `s_i` to be the label of the current task and consider each
-    song previous to s_i to be known by the system. Therefore, p_i = [s_1, s_2, ..., s_i-1]
-    will the playlist known to the system when predicting s_i to be the highest scoring song.
-    """
+    ###
+    # User u has a playlist ordered based on time. When picking an entry 
+    # in the dataset, we randomly select say ith entry to be the label of
+    # the current task and then consider each song from 0 to i-1 to be known
+    # by the system. Therefore, all the songs numbered from 0 to i-1 will known
+    # to the system when predicting ith song to be the highest scoring song.
+    ###
 
     def __init__(self, user_song, playlist, users, songs, user_enc, song_enc, behavior_enc,
         subset_size, Lt_playlist_len, targets):
+        ### initializing variables
         self.user_song = user_song
         self.playlist = playlist
         self.users = users
         self.songs = songs
-
         self.user_enc = user_enc
         self.song_enc = song_enc
         self.behavior_enc = behavior_enc
@@ -24,26 +25,13 @@ class MRSDataset(Dataset):
         self.targets = targets
 
     def __getitem__(self, index):
-        """
-        user_song is an ordered list containing user-song pairs. It is the source of truth
-        for picking training points:
-            [0: (user, song), ..., index: (user_id, label_id), ..., n: (user, song)]
-                                                    |
-                                                   u, s
-        From there, we locate the song's position in the user's complete playlist:
-            [s_1, ...,  s_k  ...,  s_i-1,  s_i,  ...,  s_n]
-                         \___________/      |
-                       partial playlist  label=label_id
-        This partial playlist will be used in two different ways: merged in the encoding of the
-        user and encoded as a sequences of varying length which will be fed to a RNN.
-        Returns
-        ---
-        Returned vectors are: (user, playlist, behaviors, subset)
-            user: the user static encoding
-            playlist: sequence of song encodings
-            behaviors: sequence of behavior encodings
-            subset: a sample list of song encodings including the label in 1st position
-        """
+        ###
+        # This function forms playlist of songs for each user, which is further merged in the encoding 
+        # of the user and encoded as a sequences of varying length which will later be fed to a RNN.
+        ###
+        
+        ### user_song is an ordered list containing user-song pairs
+        ### picking training points from user_song
         user_id, label_id = self.user_song[index]
         playlist = []
         behaviors = []
@@ -53,31 +41,34 @@ class MRSDataset(Dataset):
             playlist.append(song_id)
             behaviors.append(behavior)
 
-        # Encode both the song's metadata and the user's behavior when listening to that song.
+        ### Encode both the song's metadata and the user's behavior when listening to that song.
         for i, (song, behavior) in enumerate(zip(playlist, behaviors)):
             playlist[i] = self.encode_song(song)
             behaviors[i] = self.behavior_enc[behavior]
-
+        
+        ### user static encoding
         user = self.encode_user(user_id, playlist)
-
+        
+        ### sequence of song encodings
         playlist_Lt = playlist[-self.Lt_playlist_len:]
+        
+        ### sequence of behavior encodings
         behaviors_Lt = behaviors[-self.Lt_playlist_len:]
 
-        # Randomly sample a subset from the complete list of songs.
         subset_ids = random.sample(self.songs['song_id'].tolist(), self.subset_size - 1)
+
+        ### a sample list of song encodings including the label in 1st position
         subset = [self.encode_song(song_id) for song_id in subset_ids]
-
         label = self.encode_song(label_id)
-
+        
+        ###return user, playlist_Lt, behaviors_Lt, self.encode_song
         return user, playlist_Lt, behaviors_Lt, [label] + subset, self.targets[index]
-        #return user, playlist_Lt, behaviors_Lt, self.encode_song
+        
 
     def encode_user(self, user_id, playlist):
-        #return self.user_enc(*self.users[user_id], playlist)
         return self.user_enc[user_id]
 
     def encode_song(self, song_id):
-        #return self.song_enc(*self.songs[song_id])
         return self.song_enc[song_id]
 
     def __len__(self):
--- train.py
+++ train.py
@@ -1,17 +1,18 @@
+###imports python libraries
 from statistics import mean
-
 import torch
 import torch.nn as nn
 import numpy as np
 from torch.nn import Module, Linear
 from tqdm import tqdm
-
+### imports
 from Model.dnn import DNN
 from Model.rnn import RNN
 
 class DTNMRWrapper:
 
     def __init__(self, model, train_dl, valid_dl, optimizer):
+        ### initializing variables
         self.model = model
         self.train_dl = train_dl
         self.valid_dl = valid_dl
@@ -19,6 +20,9 @@ class DTNMRWrapper:
         self.criterion = nn.CrossEntropyLoss()
 
     def train(self, epochs):
+        ###
+        # Calls train_epoch for the number of times(ephocs) the model needs to be trained
+        ###
         for epoch in range(epochs):
             print('Epoch %i/%i' % (epoch+1, epochs))
             losses, accuracies = self.train_epoch()
@@ -26,19 +30,18 @@ class DTNMRWrapper:
                 .format(mean(losses), mean(accuracies)))
 
     def train_epoch(self):
+        ###
+        # Trains model on the enitre train_dl (training data)
+        ###
+        
         losses, accuracies = [], []
         for i, x in (t := tqdm(enumerate(self.train_dl), total=len(self.train_dl))):
-            #y = torch.zeros((np.array(x[0]).shape[0],1), dtype=torch.long) #? add , 1) if necessary
-            #y = torch.zeros((1,1), dtype = torch.long)
-            #print(x[0][-1])
             y = (torch.LongTensor(np.array([x[0][-1]]))).resize_(1,1)
             y_hat = self.model(*x[0])
-            #print(y_hat)
             self.optimizer.zero_grad()
             loss = self.criterion(y_hat, y)
             loss.backward()
             self.optimizer.step()
-
             loss = loss.item()
             if (x[0][-1] == 1):
                 accuracy = (torch.argmax(y_hat, dim=1) == 0).float().mean().item()
@@ -51,20 +54,14 @@ class DTNMRWrapper:
 
 
 class DTNMR(Module):
-    """
-    The recommendation neural network is comprised of 4 components.
-        [USFC] User Static Feature Component
-            USFC(user_encoding) = user_static_embedding
-        [MFC] Music Feature Component:
-            song_embedding = MFC(song_encoding)
-        [UDFC] User Dynamic Feature Component: long-term & short-term
-            song_playlist_embedding = UDFC(song_embedding[])
-        [RC] Rating Component
-            rating = RC(user_embedding, song_embedding)
-    """
+    ###
+    # DTMNR model contains 4 components: - User Static Feature Component(USFC), Music Feature Component(MFC)
+    # User Dynamic Feature Component for both long-term & short-term (UDFC) And the Rating Component(RC)
+    ###
 
     def __init__(self, user_mhe_size, song_mhe_size, behavior_mhe_size, st_playlist_len, emb_size):
         super(DTNMR, self).__init__()
+        ### initializing variables
         self.user_mhe_size = user_mhe_size
         self.song_mhe_size = song_mhe_size
         self.emb_size = emb_size
@@ -77,13 +74,14 @@ class DTNMR(Module):
         self.RC = Linear(2*emb_size, 1)
 
     def forward(self, user, playlist, behaviors, subset, target):
-        """
-        The forward pass of the model is the calculation of the rating between the user and
-        each song in the subset. The result is the list [Score(u, s) for s in subset].
-        """
-        # User static feature embedding.
+        ###
+        # This calculates the rating between the user and each song in the subset. 
+        # The result is the list [Score(u, s) for s in subset].
+        ###
+        
+        ### User_static feature embedding.
         user_static = self.USFC((torch.tensor(user)).float())
-        # User dynamic feature embedding.
+        ### User_dynamic feature embedding.
         if (len(playlist) == 0):
             latent_playlist = torch.zeros((1,32), dtype = torch.float)
         else:
@@ -96,13 +94,13 @@ class DTNMR(Module):
         st_playlist = Lt_playlist[-self.st_playlist_len:]
         user_dynamic = self.UDFC((Lt_playlist.unsqueeze(1)).float()) + self.UDFC((st_playlist.unsqueeze(1)).float())
 
-        # User feature combined embedding.
+        ### User_feature combined embedding.
         user_feature = user_static + user_dynamic
-        # Rating evaluation on song subset.
+        
+        ### Rating on the song subset.
         ratings = []
         for song in subset:
             embedding = self.MFC(torch.tensor(song).float())
             combined = torch.cat([user_feature, embedding.unsqueeze(0)], dim=1)
             ratings.append(self.RC(combined))
-
         return torch.stack(ratings, dim=1)
